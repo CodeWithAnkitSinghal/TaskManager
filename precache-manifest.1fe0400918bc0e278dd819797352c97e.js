df8ca8c9168854860b8f81c316d907ce6a7d54e9
Ankit Singhal <ankitsinghal@Etechaces.com>
2020-09-13 18:13:05 +0530

    Updates

---
+++ precache-manifest.1fe0400918bc0e278dd819797352c97e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db60c0bb19be8bc26e14c31da9aa6ea7",
+    "url": "/TaskManager/index.html"
+  },
+  {
+    "revision": "b787bc80a9500773060a",
+    "url": "/TaskManager/static/css/main.38ef9b20.chunk.css"
+  },
+  {
+    "revision": "fd778b70b3cb9abc2c35",
+    "url": "/TaskManager/static/js/2.b825162f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TaskManager/static/js/2.b825162f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b787bc80a9500773060a",
+    "url": "/TaskManager/static/js/main.1e454625.chunk.js"
+  },
+  {
+    "revision": "976727318ed556294b8e",
+    "url": "/TaskManager/static/js/runtime-main.40c497b1.js"
+  }
+]);
